8d927af681a0f9b0777f24dc67a566756965f818
Dawu <dawuu102@gmail.com>
2024-07-12 17:42:42 +0200

    Add files via upload

--- PinguUnfryer.py
+++ PinguUnfryer.py
@@ -7,6 +7,7 @@ import sys
 #Defines Variables
 banner = pyfiglet.figlet_format("Pingu Un-Fryer 3000")
 bannerwelcome = pyfiglet.figlet_format("Welcome Space")
+bannersteven = pyfiglet.figlet_format("Welcome Steven")
 hour = randint(0,24)
 minute = randint(0,59)
 system = platform.system()
@@ -27,14 +28,49 @@ print(machine)
 print(50*"#")
 
 #Defines all functions
-def StevenProgramKill():
-    print("noooo steven badbadbadbad")
-    time.sleep(3)
+def Steven():
+    print("Logging in... Please wait.")
+    time.sleep(2)
+    print(50*"#")
+    print(bannersteven)
+    print(50*"#")
+    StevenYN = input("Welcome Steven. Do you wish to unfry Pingu (U) or Re-Fry him? (R):" )
+    if StevenYN == "U":
+        UnfryQ()
+    elif StevenYN == "R":
+        Refry()
+    else:
+        print("Please type U or R next time. Logging Out...")
+        time.sleep(2)
+        exit()
+        
+def Refry():
+    RefryQ = input("Do you really want to deep re-fry Pingu? (Y/N)? ")
+    if RefryQ == "Y":
+        print("Refrying Pingu, please wait...")
+        time.sleep(2)
+        print("Wait...")
+        time.sleep(2)
+        print("Why would you fry Pingu? Why are you so mean...?")
+        time.sleep(3)
+        print("You made me angwy. I will remove system32 from your pc.")
+        time.sleep(1)
+        print("Enjoy some music in those last moments with your PC.")
+        print("Music will be added in next version so stay tuned - dawu")
+        time.sleep(2)
+        print("Oh, sorry nevermind. I will just kill the program instead.")
+        exit()
+    elif RefryQ == "N":
+        ProgramKill2()
+    else:
+        ProgramKill3()
+        
+def ProgramKill3():
+    print("Thats not an option. Killing program...")
     exit()
 
-
 def ProgramKill():
-    print("No Access...")
+    print("No. You don't have access.")
     time.sleep(3)
     exit()
     
@@ -49,7 +85,7 @@ def PinguUnfry():
     for i in range(5):
         print("Working, Please Wait...")
         time.sleep(3)
-    print("DONE! UNFRYING PINGU ENDED WITH SUCCESS!!! (Code: 0x0)")
+    print("DONE! UNFRYING PINGU ENDED SUCCESSFULLY!!! (Code: 0x0)")
     time.sleep(3)
     exit()
           
@@ -59,7 +95,10 @@ def Continue():
     print(50*"#")
     print(bannerwelcome)
     print(50*"#")
-    PinguYN = input("Welcome. Pingu was last fried today at #General. Do you wish to unfry him? (Y/N)")
+    UnfryQ()
+
+def UnfryQ():
+    PinguYN = input("Pingu was last fried today at #General. Do you wish to unfry him? (Y/N)")
     if PinguYN == "Y":
         PinguUnfry()
     else:
@@ -76,9 +115,11 @@ if username == "SpaceIDragon":
         time.sleep(3)
         exit()
 elif username == "RealStevenHaha":
-    StevenProgramKill()
-else:
-    ProgramKill()
+    password = input("Enter Password: ")
+    if password == "DontFryThePenguin":
+        Steven()
+    else:
+        ProgramKill()
 
 
 
